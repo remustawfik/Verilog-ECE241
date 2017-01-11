8894c30aa8729e3f784ef87714beef325c8acf6c
Reem Tawfik <reemayman@gmail.com>
2017-01-10 22:39:20 -0500

    Updating Lab4Part2
    Changing the naming of certain variables to be more comprehensive

--- Lab4Part2.v
+++ Lab4Part2.v
@@ -6,7 +6,7 @@ module Lab4Part2(LEDR, SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
 	 input [9:0] SW;
 	 input [3:0] KEY;
 	 output [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5;
-    output [9:0] LEDR;
+         output [9:0] LEDR;
 	 
 	 wire [3:0] A;
 	 wire [3:0] B;
@@ -17,9 +17,9 @@ module Lab4Part2(LEDR, SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
 	 wire Clock;
 	 wire reset_b;
 	 wire [7:0] q_ate_bit;
-    reg [7:0] ALUout;
+         reg [7:0] ALUout;
 	 
-    assign Clock = KEY[0];
+         assign Clock = KEY[0];
 	 assign A = SW[3:0];
 	 assign cin = SW[8];
 	 assign reset_b = SW[9];
@@ -32,99 +32,94 @@ module Lab4Part2(LEDR, SW,KEY,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
 	 
 
 	 
-	 HEX_NUM PUPPER0 (
-						.a(A[3]),
-						.b(A[2]),
-						.c(A[1]),
-						.d(A[0]),
-						.H0(HEX0[0]),
-						.H1(HEX0[1]),
-						.H2(HEX0[2]),
-						.H3(HEX0[3]),
-						.H4(HEX0[4]),
-						.H5(HEX0[5]),
-						.H6(HEX0[6]),
+	 HEX_NUM block0 (
+			  .a(A[3]),
+		          .b(A[2]),
+			  .c(A[1]),
+			  .d(A[0]),
+			  .H0(HEX0[0]),
+			  .H1(HEX0[1]),
+			  .H2(HEX0[2]),
+			  .H3(HEX0[3]),
+			  .H4(HEX0[4]),
+			  .H5(HEX0[5]),
+			  .H6(HEX0[6]),
 						
-						);
+			);
 						
-	HEX_NUM PUPPER4 (
-						.a(q_ate_bit[3]),
-						.b(q_ate_bit[2]),
-						.c(q_ate_bit[1]),
-						.d(q_ate_bit[0]),
-						.H0(HEX4[0]),
-						.H1(HEX4[1]),
-						.H2(HEX4[2]),
-						.H3(HEX4[3]),
-						.H4(HEX4[4]),
-						.H5(HEX4[5]),
-						.H6(HEX4[6]),
+	HEX_NUM block1 (
+			  .a(q_ate_bit[3]),
+			  .b(q_ate_bit[2]),
+			  .c(q_ate_bit[1]),
+			  .d(q_ate_bit[0]),
+			  .H0(HEX4[0]),
+			  .H1(HEX4[1]),
+			  .H2(HEX4[2]),
+			  .H3(HEX4[3]),
+			  .H4(HEX4[4]),
+			  .H5(HEX4[5]),
+			  .H6(HEX4[6]),
 						
-						);
+			);
 						
 						
-	HEX_NUM PUPPER5 (
-						.a(q_ate_bit[7]),
-						.b(q_ate_bit[6]),
-						.c(q_ate_bit[5]),
-						.d(q_ate_bit[4]),
-						.H0(HEX5[0]),
-						.H1(HEX5[1]),
-						.H2(HEX5[2]),
-						.H3(HEX5[3]),
-						.H4(HEX5[4]),
-						.H5(HEX5[5]),
-						.H6(HEX5[6]),
+	HEX_NUM block2 (
+			  .a(q_ate_bit[7]),
+			  .b(q_ate_bit[6]),
+			  .c(q_ate_bit[5]),
+			  .d(q_ate_bit[4]),
+			  .H0(HEX5[0]),
+			  .H1(HEX5[1]),
+			  .H2(HEX5[2]),
+			  .H3(HEX5[3]),
+			  .H4(HEX5[4]),
+			  .H5(HEX5[5]),
+			  .H6(HEX5[6]),
 						
-						);
-	four_bit_adder doge(
-								.cin(Cin),
-								.a0(A[0]),
-								.b0(B[0]),
-								.a1(A[1]),
-								.b1(B[1]),
-								.a2(A[2]),
-								.b2(B[2]),
-								.a3(A[3]),
-								.b3(B[3]),
-								.s0(Adder_Result[0]),
-								.s1(Adder_Result[1]),
-								.s2(Adder_Result[2]),
-								.s3(Adder_Result[3]),
-								.cout(Adder_Result[4])
+			);
+	four_bit_adder block3(
+			  .cin(Cin),
+			  .a0(A[0]),
+			  .b0(B[0]),
+			  .a1(A[1]),
+			  .b1(B[1]),
+			  .a2(A[2]),
+			  .b2(B[2]),
+			  .a3(A[3]),
+			  .b3(B[3]),
+			  .s0(Adder_Result[0]),
+			  .s1(Adder_Result[1]),
+			  .s2(Adder_Result[2]),
+			  .s3(Adder_Result[3]),
+			  .cout(Adder_Result[4])
 								
-								);
+			);
 	
 
-synch_set_register smol(
-								.d(ALUout),
-								.Clock(~Clock),
-								.Reset_b(reset_b),
-								.q(q_ate_bit),
+       synch_set_register block4(
+			  .d(ALUout),
+			  .Clock(~Clock),
+			  .Reset_b(reset_b),
+			  .q(q_ate_bit),					
+			);
 								
-								
-								);
-								
-	assign B = q_ate_bit[3:0]; 							
+assign B = q_ate_bit[3:0]; 							
 
 								
 always@(*)
 	begin
 	case(~ALUinput)
-	3'b000: ALUout = {zero, Adder_Result}; //works
-	3'b001: ALUout = A+B;//wtf
-	3'b010: ALUout = ({A,B})? 8'b01111110 : 8'b00000000; //works
-	3'b101: ALUout = {(A^B),(A|B)}; //works
-	3'b011: ALUout = (|{A,B})? 8'b10000001 : 8'b00000000; //works
+	3'b000: ALUout = {zero, Adder_Result}; 
+	3'b001: ALUout = A+B;
+	3'b010: ALUout = ({A,B})? 8'b01111110 : 8'b00000000; 
+	3'b101: ALUout = {(A^B),(A|B)}; 
+	3'b011: ALUout = (|{A,B})? 8'b10000001 : 8'b00000000; 
 	3'b100: ALUout = {A,B<<A}; 
 	3'b110: ALUout = A*B;
 	3'b111: ALUout = q_ate_bit; 
 	default: ALUout = 8'b00000000;
 	endcase
 	end
-	
-	
-	
 endmodule
 
 
@@ -147,9 +142,7 @@ module synch_set_register(d,Clock,Reset_b,q);
 			begin	
 			q <= d;
 		   	// value of d passes through to output q
-			end
-
-		
+			end	
 	end
    
 endmodule 
@@ -165,7 +158,6 @@ module four_bit_adder(cin,a0,b0,a1,b1,a2,b2,a3,b3,s0,s1,s2,s3,cout);
 	full_adder FA3(a2,b2,c2,s2,c3);
 	full_adder FA4(a3,b3,c3,s3,cout);
 	
-
 endmodule
 
 module full_adder(a,b,ci,s,co);
@@ -194,13 +186,13 @@ module HEX_NUM(a,b,c,d, H0,H1,H2,H3,H4,H5,H6);
 	input a,b,c,d;
 	output H0,H1,H2,H3,H4,H5,H6;
 	
-   h0_block DOGGO0(a, b, c, d,H0);
-   h1_block DOGGO1(a, b, c, d,H1);
-   h2_block DOGGO2(a, b, c, d,H2);
-   h3_block DOGGO3(a, b, c, d,H3);
-   h4_block DOGGO4(a, b, c, d,H4);
-   h5_block DOGGO5(a, b, c, d,H5);
-	h6_block DOGGO6(a, b, c, d,H6);
+   h0_block HB0(a, b, c, d,H0);
+   h1_block HB1(a, b, c, d,H1);
+   h2_block HB2(a, b, c, d,H2);
+   h3_block HB3(a, b, c, d,H3);
+   h4_block HB4(a, b, c, d,H4);
+   h5_block HB5(a, b, c, d,H5);
+   h6_block HB6(a, b, c, d,H6);
 	
 endmodule
 	
